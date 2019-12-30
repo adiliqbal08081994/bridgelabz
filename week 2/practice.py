aeeb41139c863a554d203983b95d29743ac00d49
adil <adiliqbal10@gmail.com>
2019-12-30 12:36:30 +0530

    [Adil]2Darray DS complete

--- week 2/practice.py
+++ week 2/practice.py
@@ -1,13 +1,12 @@
 import ctypes
-
- class _ArrayIterator():
+class _ArrayIterator():
         def __init__(self,theArray):
             self._arrayref=theArray
             self.curndx=0
         def __iter__(self):
             return self
         def __next__(self):
-            if self._curndx<len(self._arrayref):
+            if self.curndx<len(self._arrayref):
                 entry=self._arrayref[self.curndx]
                 self.curndx += 1
                 return entry
@@ -36,3 +35,39 @@ class Array:
     
     def __iter__(self):
         return _ArrayIterator(self.elements)
+
+
+
+class Array2D:
+    def __init__(self,numRows,numCols):
+        self._TheRows=Array(numRows)
+
+    for i in range(numRows):
+        self._TheRows[i]=Array(numCols)
+    def numRows(self):
+        return len(self._TheRows)
+    def numCols(self):
+        return len(self._TheRows[0])
+    def clear(self,value):
+        for row in range(self.numRows()):
+            row.clear(value)
+
+    def __getitem__(self,ndxTuple):
+        assert len(ndxTuple)==2
+        row=ndxTuple[0]
+        col=ndxTuple[1]
+        assert row>0 and row<self.numRows()and col>0 and col<self.numCols()
+        the1Darray=self._TheRows[row]
+        return the1Darray[col]
+
+    def __setitems__(self,ndxTuple,Value):
+        assert len(ndxTuple)==2
+        row=ndxTuple[0]
+        col=ndxTuple[1]
+        assert row>0 and row<self.numRows()and col>0 and col<self.numCols()
+        the1Darray=self._TheRows[row]
+        the1Darray[col]=Value
+
+
+
+
