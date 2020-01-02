d7feb547c93b45cfccd3d291534f647e0fe1e8e4
adil <adiliqbal10@gmail.com>
2020-01-02 12:15:18 +0530

    [adil] performing basic operations on linked list

--- week 2/test.py
+++ week 2/test.py
@@ -1,9 +1,158 @@
 import sys
 sys.path.append('/home/user/Desktop/task/week 1/algorithim/week 2')
 
-from practice import Array
-s=Array(10)
-print(s.__len__())
-s.__setitem__(0,2)
-s.__setitem__(1,5)
-s.__setitem__(2,7)
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class listnode:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+
+# In[2]:
+
+
+a=listnode(5)
+b=listnode(10)
+c=listnode(15)
+d=listnode(20)
+e=listnode(25)
+f=listnode(30)
+
+
+# In[3]:
+
+
+a.next=b
+b.next=c
+c.next=d
+d.next=e
+e.next=f
+
+
+# In[4]:
+
+
+a.next.next.data
+
+
+# In[5]:
+
+
+b,c=None,None
+
+
+# In[6]:
+
+
+a.next.data
+
+
+# In[7]:
+
+
+a.next.next.data
+
+
+# In[8]:
+
+
+t=listnode(0)
+t.next=a
+
+
+# In[9]:
+
+
+head=t
+while head is not None:
+    print(head.data)
+    head=head.next
+
+
+# In[10]:
+
+
+curnode=a
+while curnode is not None and curnode.data!=25:
+    curnode = curnode.next
+print(curnode.data)
+
+
+# In[11]:
+
+
+t=listnode(0)
+
+
+# In[12]:
+
+
+t.next=a
+
+
+# In[13]:
+
+
+prednode=None
+curnode=t
+while curnode is not None and curnode.data!=25:
+    prednode=curnode
+    curnode=curnode.next
+prednode.next=curnode.next
+
+
+# In[14]:
+
+
+head=t
+while head is not None:
+    print(head.data)
+    head=head.next
+
+
+# In[15]:
+
+
+new=listnode(35)
+
+
+# In[16]:
+
+
+tail=a
+while tail.next is not None:
+    tail=tail.next
+tail.next=new
+
+
+# In[17]:
+
+
+ins=listnode(25)
+
+
+# In[18]:
+
+
+pred=None
+curnode=t
+while curnode is not None and curnode.data!=30:
+    pred=curnode
+    curnode=curnode.next
+pred.next=ins
+ins.next=curnode
+
+
+# In[19]:
+
+
+head=t
+while head is not None:
+    print(head.data)
+    head=head.next
+
